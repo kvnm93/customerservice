cb9b906e74d42dfc81a5e50a8f9859e6f8138f29
kevinmoisch <kevinmoisch@gmail.com>
2020-03-08 16:11:42 +0100

    Eureka Client Config

--- src/main/java/de/leuphana/customerservice/component/behaviour/CustomerService.java
+++ src/main/java/de/leuphana/customerservice/component/behaviour/CustomerService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -15,6 +16,7 @@ import javax.annotation.PostConstruct;
 import java.util.Optional;
 
 @SpringBootApplication
+@EnableEurekaClient
 @ComponentScan(basePackages = {"de.leuphana"})
 @EntityScan( basePackages = {"de.leuphana"})
 @EnableJpaRepositories(basePackages = "de.leuphana.customerservice.component.structure")
